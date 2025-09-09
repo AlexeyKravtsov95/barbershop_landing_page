a9f5bd69ff8a83d22ba37b8a01dd90c05d81399c
AlexeyKravtsov95 <alexey.kravtsov95@gmail.com>
2025-09-09 09:03:15 +0700

    Added issue

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.masters__sliders').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        infinite: true,
+        dots: true,
+    })
+
+    $('.socials__slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        infinite: true,
+        dots: true,
+    })
+})
